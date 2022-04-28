7854230816f6d48175e166c94c3db8c5c2f44367
shremahe <shremahe@cisco.com>
2022-04-28 16:49:07 +0530

    Added code for load testing

--- __main__.py
+++ __main__.py
@@ -18,6 +18,7 @@ ENDPOINT = regdata['clientEndPoint']
 SUBTOPIC= regdata['subscribeTopic']
 PUBTOPIC= regdata['publishTopic']
 MESSAGE = json.dumps({"remoteNodeId": CLIENT_ID , "transactionType": "INITIAL-SETUP" , "topic": AGENTGATEWAY_QUEUE ,  "customerId": CUSTOMER_ID , "s3Bucket": "agentrepo/2.0.0" })
+#MESSAGE = json.dumps({"remoteNodeId":CLIENT_ID, "transactionType":"CLI_UPLOADS","transactionId":"null","customerId":"82EPpi6z0QinZX","topic":"afm-cli_data","status":"Success","details":[{"message":"{\"fileName\":\"20039_SIM1296590_82EPpi6z0QinZX_1649251398253_clioutput.txt\",\"serialNumber\":\"SIM1296590\",\"prefix\":\"/st=Campus/cli/custId=82EPpi6z0QinZX/ctrlId=2563fb5c-ce22-4725-be88-7f70a790e359/date=2022-04-22/devId=9c86a46c-8bfc-327a-848e-2088b84d13c1/\",\"customerId\":\"82EPpi6z0QinZX\",\"msgId\":20039,\"ng\":\"true\"}"}]})
 
 # Spin up resources
 event_loop_group = io.EventLoopGroup(1)
@@ -32,7 +33,7 @@ myMQTTClient = mqtt_connection_builder.mtls_from_path(
             client_bootstrap=client_bootstrap,
             ca_filepath=PATH_TO_AMAZON_ROOT_CA_1,
             client_id=CLIENT_ID,
-            clean_session=False,
+            clean_session=True,
             keep_alive_secs=6
             )
 
@@ -62,6 +63,7 @@ received_all_event = threading.Event()
 print("Connected!")
 # Publish message to server desired number of times.
 print('Begin Publish')
+#while True:
 myMQTTClient.publish(topic=PUBTOPIC, payload=MESSAGE, qos=mqtt.QoS.AT_LEAST_ONCE)
 print("Published: " + MESSAGE)
 print('Publish End')
